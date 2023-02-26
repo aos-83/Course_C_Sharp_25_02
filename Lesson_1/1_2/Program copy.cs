925d026a258332f6c722f0790c3c29598e2fd52f
olga_avdukova <aos-83@ya.ru>
2023-02-26 18:36:25 +0300

    test task 1_2

--- Lesson_1/1_2/Program copy.cs
+++
@@ -1,6 +0,0 @@
-﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
-
-// string s_a = Console.ReadLine()!;
-int a = int.Parse(Console.ReadLine()!);
-
-Console.WriteLine(a * a);
--- Lesson_1/1_2/Program.cs
+++ Lesson_1/1_2/Program.cs
@@ -4,8 +4,40 @@ string s_a = Console.ReadLine()!;
 
 if (s_a == "1")
 {
-    Console.WriteLine("Понедельник");
+    Console.WriteLine("пн");
 }
- else if (s_a == "2")
+
+else if (s_a == "2")
 {
-    Console.WriteLine("Вторник");
+    Console.WriteLine("вт");
+}
+
+else if (s_a == "3")
+{
+    Console.WriteLine("cр");
+}
+
+else if (s_a == "4")
+{
+    Console.WriteLine("чт");
+}
+
+else if (s_a == "5")
+{
+    Console.WriteLine("пт");
+}
+
+else if (s_a == "6")
+{
+    Console.WriteLine("сб");
+}
+
+else if (s_a == "7")
+{
+    Console.WriteLine("вс");
+}
+
+else
+{
+    Console.WriteLine("err");
+}
