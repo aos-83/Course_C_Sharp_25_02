0959dbad7aafbb9a7f64f7cf0417c1b1b770ff37
olga_avdukova <aos-83@ya.ru>
2023-02-27 10:33:49 +0300

    HW_task_1_3

---
+++ Lesson_1/HW/HW_1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+//(делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число ");
+string s_a = Console.ReadLine()!;
+int a = int.Parse(s_a);
+int remDiv = a % 2;
+
+
+if (a == 0)
+{
+    Console.WriteLine("Четное");
+}
+else 
+{
+   Console.WriteLine("Нечетное"); 
+}
