b55224374fc67cc27053faf7de61466e3a58cf3d
olga_avdukova <aos-83@ya.ru>
2023-03-07 19:55:42 +0300

    task_3_2

--- Lesson_3/3_1/Program.cs
+++ Lesson_3/3_1/Program.cs
@@ -6,11 +6,11 @@ void Quarters(int num)
     if (num == 1)
         Console.WriteLine("x > o, y > 0");
     else if (num == 2)
-        Console.WriteLine("x < 0 && y > 0");
+        Console.WriteLine("x < 0, y > 0");
     else if (num == 3)
-        Console.WriteLine("x < 0 && y < 0");
+        Console.WriteLine("x < 0, y < 0");
     else if (num == 4)
-        Console.WriteLine("x > 0 && y < 0");
+        Console.WriteLine("x > 0, y < 0");
     else
         Console.WriteLine("error");
 }
---
+++ Lesson_3/3_2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D
+//пространстве.
+//A (3,6); B (2,1) -> 5,09
+//A (7,-5); B (1,-1) -> 7,21
+
+double Example (double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y2-y1, 2));
+}
+
+Console.WriteLine(Example(3, 6, 2 , 1));
