6e44d822d7cc71e613e5e9e977ac5592b98bad3b
olga_avdukova <aos-83@ya.ru>
2023-03-08 17:09:04 +0300

    HW_task_4_2

---
+++ Lesson_4/HW/HW_4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+int SumNum(int num)
+{
+    int all_sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum += i;
+    }
+    return all_sum;
+}
+int A = int.Parse(Console.ReadLine()!);
+int result = SumNum(A);
+Console.WriteLine(result);
