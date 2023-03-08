3614d826163a86dde2a744a686191e8f454e20ce
olga_avdukova <aos-83@ya.ru>
2023-03-08 16:04:05 +0300

    task_4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+int PrNum(int num)
+{
+    int all_pr = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        all_pr *= i;
+    }
+    return all_pr;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result = PrNum(A);
+Console.WriteLine(result);
