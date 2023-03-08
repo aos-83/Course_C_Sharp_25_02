7a8ce251f567180abca746bb6e14f2cccb6e4bad
olga_avdukova <aos-83@ya.ru>
2023-03-08 13:00:02 +0300

    HW_task_3_2 - создана часть кода, буду править

---
+++ Lesson_3/HW/HW_3_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("Введите 3 координаты 1 точки: ");
+Console.WriteLine("Введите 3 координаты 2 точки: ");
+double Example (double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y2-y1, 2)+Math.Pow(z2-z1, 2));
+}
+int Length = int.Parse(Console.ReadLine()!);
+Example(Length);
