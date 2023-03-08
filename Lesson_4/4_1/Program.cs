b65aed20aa34873802f54b9557e769ef44d38ab5
olga_avdukova <aos-83@ya.ru>
2023-03-08 15:46:16 +0300

    Task_4_1

---
+++ Lesson_4/4_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+int SumNum(int num)
+{
+    int kol = 0;
+    for (int i = 1; num > 0; i++)
+      {
+        num = num/10;
+        kol = i;
+      }
+    return kol;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result  = SumNum(A);
+Console.WriteLine(result);
