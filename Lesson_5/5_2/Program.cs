f7706339b0c91e36ccebe840136876567b99313f
olga_avdukova <aos-83@ya.ru>
2023-03-12 17:34:48 +0300

    task_5_2 почему-то не заработал код, хотя перепроверила

--- Lesson_5/5_2/Program.cs
+++ Lesson_5/5_2/Program.cs
@@ -1,3 +1,41 @@
 ﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
 //4; массив [6, 7, 19, 345, 3] -> нет
 //3; массив [6, 7, 19, 345, 3] -> да
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+void SumPosNeg(int[] arr, int num_1)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i == num_1])
+        {
+            Console.WriteLine("Да");
+            return;
+        }
+    }
+    Console.WriteLine("Нет");
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+int num_1 = int.Parse(Console.ReadLine()!);
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+SumPosNeg(mass, num_1);
