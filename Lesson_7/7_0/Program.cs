b17155e8e997080fe9f0f6a639b56f0a9f50ab3e
olga_avdukova <aos-83@ya.ru>
2023-03-25 09:18:25 +0300

    Неудачная отладка 7_0

--- Lesson_7/7_0/Program.cs
+++ Lesson_7/7_0/Program.cs
@@ -1,8 +1,4 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
-//m = 3, n = 4.
-//1 4 8 19
-//5 -2 33 -2
-//77 3 8 1
 
 void Print(int[,] arr)
 {
@@ -20,11 +16,9 @@ int[,] MassNums(int row, int column, int from, int to)
 {
     int[,] arr = new int[row, column];
     for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < column; j++)
-            arr[i, j] = new Random().Next(from, to);
+    for (int j = 0; j < column; j++)
+    arr[i, j] = new Random().Next(from, to);
         return arr;
-    }
 }
 int num_row = int.Parse(Console.ReadLine()!);
 int num_column = int.Parse(Console.ReadLine()!);
