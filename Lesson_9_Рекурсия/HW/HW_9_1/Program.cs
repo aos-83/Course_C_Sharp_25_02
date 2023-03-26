9ee0d8ab356e2a8fae73fcddecc02ec25da5bc77
olga_avdukova <aos-83@ya.ru>
2023-03-26 10:26:29 +0300

    HW_9_1

---
+++ Lesson_9_Рекурсия/HW/HW_9_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Задайте значения M и N. Напишите программу, которая выведет все
+//чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+Console.Write("Введите значение M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine()!);
+void ValueSet(int M, int N)
+{
+    if (M > N)
+        return;
+    if (M % 2 == 0)
+    {
+        Console.Write($"{ M}, ");
+    }
+    ValueSet(M + 1, N);
+
+}
+ValueSet(M, N);
