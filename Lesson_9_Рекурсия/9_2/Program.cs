130da89ad73a303d3c8a5b1854d240ac28501444
olga_avdukova <aos-83@ya.ru>
2023-03-26 10:07:01 +0300

    task_9_2

---
+++ Lesson_9_Рекурсия/9_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задайте значения M и N. Напишите программу, которая рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+int ValuesSet(int N)
+{
+    if (N==0) return 0;
+    return ValuesSet(N / 10)+N % 10;
+}
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine(ValuesSet(num));
